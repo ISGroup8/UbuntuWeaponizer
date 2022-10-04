2fb4d45f6fa9de6ad8974aa79af5216a3fdcf923
Elady-Xu <72891840+Elady-Xu@users.noreply.github.com>
2022-10-04 16:18:31 +0200

    Update main.py

--- Codigo/main.py
+++ Codigo/main.py
@@ -4,13 +4,38 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import tkinter as tk
+import pandas as pd
+import base64
+import os
+from tkinter.filedialog import askdirectory
+from tkinter import ttk
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def fun1():
+    print("1")
+
+def fun2():
+    print("2")
+
+class Aplicacion:
+    def __init__(self):
+        self.root=tk.Tk()
+        self.root.title("Ubuntu Weaponizer")
+        self.canvas1=tk.Canvas(self.root, width=600, height=400, background="lavender")
+        self.canvas1.grid(column=0, row=0)
+        self.canvas1.create_line(100, 100, 100,1000, fill="black")
+
+        btn = tk.Button (self.root, text = "Func 1", command=fun1)
+        #btn.pack(side=tk.LEFT)
+        btn = tk.Button (self.root, text = "Func 2", command=fun2)
+        #btn.pack(side=tk.LEFT)
+        self.canvas1=tk.Canvas(self.root, width=600, height=400, background="lavender")
+        self.canvas1.create_line(0, 0, 100,50, fill="white")
+        self.root.mainloop()
+
+
+aplicacion1=Aplicacion()
+
