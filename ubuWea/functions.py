ebc9603ee9d8320e651f849decf6ba3b29080891
TheRealMcMob <114933713+Not-Adan@users.noreply.github.com>
2022-11-09 22:25:54 +0100

    mejorada funcion instalar

--- ubuWea/functions.py
+++ ubuWea/functions.py
@@ -1,13 +1,21 @@
 import os
 
-def install_apps (apt_list):
-    install = 'sudo apt install -y'
-    for item in apt_list:
-        install += ' ' + item
-    os.system(install)
 
+apt_apps = {"nmap", "ffuf", "wpscan", "sqlmap", "gdb", "wireshark", "libimage-exiftool-perl"}
+git_apps = {"setoolkit", "socialpish", "hidden-eye", "shellpish", "pyphisher", "burpsuite",
+ "rustscan", "ghidra", "cutter", "pwntools", "Radare2", "Volatility 2", "Volatility 3", "Autopsy",
+  "Metagoofil", "The Harvester", "Sherlock", "Maltego", "The Spy Job"} #añadir el comando para instalar.
 
-def install_git_app (number):
+def install_apps (l):
+    install_apt = 'sudo apt install -y'
+    for item in l:
+        if item in apt_apps:
+            install_apt += ' ' + item
+        else:
+            os.system(git_apps[item])
+
+    if install_apt != 'sudo apt install -y':
+        os.system(install_apt)
 
 
 def system_update ():
