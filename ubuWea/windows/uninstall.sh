b507ab7fdb60d10ddb933dadc5a51de71a42c6b0
McMob <114933713+Not-Adan@users.noreply.github.com>
2022-12-19 13:17:59 +0100

    Uninstall Apt done

---
+++ ubuWea/windows/uninstall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+user=$1
+uninstallpath=/home/$user/tools
+
+function setoolkit() {
+}
+
+function hidden-eye { 
+}
+
+function shellpish { 
+}
+    
+function PyPhisher { 
+}
+
+function Burpsuite {  
+}
+    
+function rustscan {  
+}
+    
+function ghidra {
+}
+
+function cutter { 
+}
+
+function pwntools { 
+}
+
+function Radare2 { 
+}
+
+function Volatility2 {
+}
+
+function the_spy_job {
+}
+
+function Sherlock {
+}
+
+function Maltego {
+}
+
+function TheHarvester {
+}
+
+function Metagoofil {
+}
+
+
+function Autopsy {
+}
+
+$2
